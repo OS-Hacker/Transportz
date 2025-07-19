72273f818aa3dbcd92fe1463478bd2b1760b8265
OS-Hacker <os363612@gmail.com>
2025-07-19 14:57:11 +0530

    Last Commit

--- backend/App.js
+++ backend/App.js
@@ -6,6 +6,9 @@ import userRouter from "./routes/user.routes.js";
 import transportRoute from "./routes/transport.routes.js";
 import bookingRouter from "./routes/booking.routes.js";
 import path from "path"
+import { fileURLToPath } from "url"; // Add this import
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config(); // Load environment variables
 
@@ -29,12 +32,11 @@ App.listen(process.env.PORT, () =>
 );
 
 // Deployment setup
-const dirPath = path.resolve();
-if (process.env.NODE_ENV === "PRODUCTION") {
-  App.use(express.static(path.join(dirPath, "../frontend/dist")));
+// Serve static files from the frontend build folder
+App.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  App.get("*", (req, res) => {
-    res.sendFile(path.join(dirPath, "../frontend", "dist", "index.html"));
-  });
-}
+// All other routes should redirect to the frontend's index.html
+App.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
